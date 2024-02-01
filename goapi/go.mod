dc6bbe9a1e17c1f17afe490f222066dfa1b2cfe9
Alisson Santana <alissonsantana16608@gmail.com>
2024-01-31 22:32:54 -0300

    Microsserviço Api

--- goapi/go.mod
+++ goapi/go.mod
@@ -2,4 +2,12 @@ module github.com/alissonsantana154/sistema-ecommerce/goapi
 
 go 1.21.6
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/go-chi/chi/v5 v5.0.11
+	github.com/google/uuid v1.6.0
+)
+
+require (
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+)
--- goapi/internal/database/product_db.go
+++ goapi/internal/database/product_db.go
@@ -15,7 +15,7 @@ func NewProductDB(db *sql.DB) *ProductDB {
 }
 
 func (pd *ProductDB) GetProducts() ([]*entity.Product, error) {
-	rows, err := pd.db.Query("SELECT id, name, price, category_id FROM products")
+	rows, err := pd.db.Query("SELECT id, name, description, price, category_id, image_url FROM products")
 	if err != nil {
 		return nil, err
 	}
@@ -24,7 +24,7 @@ func (pd *ProductDB) GetProducts() ([]*entity.Product, error) {
 	var products []*entity.Product
 	for rows.Next() {
 		var product entity.Product
-		if err := rows.Scan(&product.ID, &product.Name, &product.Price, &product.CategoryID); err != nil {
+		if err := rows.Scan(&product.ID, &product.Name, &product.Description, &product.Price, &product.CategoryID, &product.ImageURL); err != nil {
 			return nil, err
 		}
 		products = append(products, &product)
@@ -34,7 +34,8 @@ func (pd *ProductDB) GetProducts() ([]*entity.Product, error) {
 
 func (pd *ProductDB) GetProduct(id string) (*entity.Product, error) {
 	var product entity.Product
-	err := pd.db.QueryRow("SELECT id, name, price, category_id, image_url, FROM products WHERE id = ?", id).Scan(&product.Name, &product.Price, &product.CategoryID, &product.ImageURL)
+	err := pd.db.QueryRow("SELECT id, name, price, category_id, image_url FROM products WHERE id = ?", id).
+		Scan(&product.ID, &product.Name, &product.Price, &product.CategoryID, &product.ImageURL)
 	if err != nil {
 		return nil, err
 	}
@@ -42,7 +43,7 @@ func (pd *ProductDB) GetProduct(id string) (*entity.Product, error) {
 }
 
 func (pd *ProductDB) GetProductByCategoryID(categoryID string) ([]*entity.Product, error) {
-	rows, err := pd.db.Query("SELECT id, name, description, price, category_id, image_url FROM products WHERE category_id = ?", categoryID)
+	rows, err := pd.db.Query("select id, name, description, price, category_id, image_url from products where category_id = ?", categoryID)
 	if err != nil {
 		return nil, err
 	}
@@ -60,7 +61,8 @@ func (pd *ProductDB) GetProductByCategoryID(categoryID string) ([]*entity.Produc
 }
 
 func (pd *ProductDB) CreateProduct(product *entity.Product) (*entity.Product, error) {
-	_, err := pd.db.Exec("INSERT INTO products (id, name, description, price, category_id, image_url) VALUE (?, ?, ?, ?, ?, ?)", product.ID, product.Name, product.Description, product.Price, product.CategoryID, product.ImageURL)
+	_, err := pd.db.Exec("INSERT INTO products (id, name, description, price, category_id, image_url) VALUES (?, ?, ?, ?, ?, ?)",
+		product.ID, product.Name, product.Description, product.Price, product.CategoryID, product.ImageURL)
 	if err != nil {
 		return nil, err
 	}
